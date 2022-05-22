860031556e72400f422a70ae37086fab41fb7a67
cagdasaydemir <ac.cagdasaydemir@gmail.com>
2022-05-22 18:40:02 +0300

    added authentication
    -added token for authentication

--- src/utils/interceptors.js
+++ src/utils/interceptors.js
@@ -2,10 +2,13 @@ import axios from "axios";
 
 axios.interceptors.request.use(
  config => {
-  
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkFiZHVsbGFoIiwibmJmIjoxNjUyNDU3NzEzLCJleHAiOjE2NTI1NDQxMTMsImlhdCI6MTY1MjQ1NzcxMywiaXNzIjoiYnVyYXPEsW7EsSBhbmxhbWHEsWRtIiwiYXVkIjoiVG9kb0FwaSJ9.xJrdkWS_x3HSOWKqpOsqJ4xaX_zCEnzWSwwkr8jCU_s";
 
    config.baseURL = process.env.REACT_APP_API_URL;
   
+   if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
    return config;
  },
